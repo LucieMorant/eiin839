db4d2ff3e0c88588f8e0def5684fb3a2dc5ca978
Lucie <lucie.morant@etu.univ-cotedazur.fr>
2021-02-18 16:31:26 +0100

    Rendu question 4 et 5

--- TD2/ExecTest/Program.cs
+++ TD2/ExecTest/Program.cs
@@ -8,6 +8,13 @@ namespace ExeTest
         {
             if (args.Length == 1)
                 Console.WriteLine(args[0]);
+            else if (args.Length > 1)
+            {
+                for (int i = 1; i < args.Length; i++)
+                {
+                    Console.WriteLine(args[i]);
+                }
+            }
             else
                 Console.WriteLine("ExeTest <string parameter>");
         }
---
+++ TD2/WebDynamic/Mymethods.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Web;
+using System.Diagnostics;
+using System.IO;
+
+namespace BasicServerHTTPlistener
+{
+	class Mymethods
+	{
+		private Uri uri;
+
+		public Mymethods(Uri uri)
+		{
+			this.uri = uri;
+		}
+
+		public System.Collections.Specialized.NameValueCollection getParameters()
+        {
+			return HttpUtility.ParseQueryString(uri.Query);
+        }
+
+		public String myMethods(System.Collections.Specialized.NameValueCollection parameters)
+        {
+			String response = "<HTML><BODY> Hello ";
+			int paramNb = 0;
+			foreach(string key in parameters.AllKeys)
+            {
+				string[] values = parameters.GetValues(key);
+				foreach(string value in values)
+                {
+					if (paramNb == 0) response += " " + value;
+					else response += " et " + value;
+                }
+				paramNb++;
+            }
+			response += "</BODY></HTML>";
+			return response;
+        }
+
+		public String myExternalMethods(System.Collections.Specialized.NameValueCollection parameters)
+        {
+                ProcessStartInfo start = new ProcessStartInfo();
+                start.FileName = @"C:\Users\Lucie\Documents\Polytech\SEMESTRE4\WebSOC\eiin839\TD2\BasicExamplesTD2\ExecTest\bin\Debug\ExecTest.exe";
+                int paramNb = 0;
+                foreach (string key in parameters.AllKeys)
+                {
+                    string[] values = parameters.GetValues(key);
+                    foreach (string value in values)
+                    {
+                        start.Arguments += value + " ";
+                    }
+                    paramNb++;
+                }
+                start.UseShellExecute = false;
+                start.RedirectStandardOutput = true;
+                String result;
+
+                using (Process process = Process.Start(start))
+                {
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        result = reader.ReadToEnd();
+                }
+                }
+            return result;
+            }
+        }
+    }
