447122564207611374fc96ef20d9f2b947e408b6
Lucie <lucie.morant@etu.univ-cotedazur.fr>
2021-03-10 16:59:52 +0100

    SOAP Ws et Exercice 1 et 2 First UI Ws Client

---
+++ webFront/WsSOAP/WsSOAP/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ServiceReference1;
+
+namespace WsSOAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorSoapClient calculator = new CalculatorSoapClient(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
+            Console.WriteLine("2 + 3 = {0}", calculator.AddAsync(2,3).Result);
+        }
+    }
+}
--- webFront/materials/main.js
+++ webFront/materials/main.js
@@ -1,1 +1,16 @@
-document.getElementById("workingTest").innerHTML="It works";
+function retrieveAllContracts() {
+    var targetUrl = "https://api.jcdecaux.com/vls/v3/contracts?apiKey=" + document.getElementById("apiKey").value;
+    var requestType = "GET";
+
+    var caller = new XMLHttpRequest();
+    caller.open(requestType, targetUrl, true);
+    caller.setRequestHeader("Accept", "application/json");
+    caller.onload = contractsRetrieved;
+
+    caller.send();
+}
+
+function contractsRetrieved() {
+    var response = JSON.parse(this.responseText);
+    console.log(response);
+}
