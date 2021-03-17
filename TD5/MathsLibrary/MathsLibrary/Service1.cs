0c1692f33ae2061e80f2697894e5c0bbfaf0bf03
Lucie <lucie.morant@etu.univ-cotedazur.fr>
2021-03-17 16:50:19 +0100

    TD5 Introduction et SOAP Web Services
    TD5 Introduction et SOAP Web Services

---
+++ TD5/MathsLibrary/MathsLibrary/Service1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MathsLibrary
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
+    public class Service1 : IMathsOperations
+    {
+        public int Add(int arg1, int arg2)
+        {
+            return arg1 + arg2;
+        }
+
+        public int Multiply(int arg1, int arg2)
+        {
+            return arg1 * arg2;
+        }
+
+        public int Subtract(int arg1, int arg2)
+        {
+            return arg1 - arg2;
+        }
+    }
+}
---
+++ TD5/MathsLibrary/MathsLibraryService/Program.cs
@@ -0,0 +1,14 @@
+﻿using ServiceReference2;
+using System;
+
+namespace MathsLibraryService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathsOperationsClient client = new MathsOperationsClient(MathsOperationsClient.EndpointConfiguration.BasicHttpBinding_IMathsOperations); 
+            Console.WriteLine("1+2=" + client.AddAsync(1, 2).Result);
+        }
+    }
+}
